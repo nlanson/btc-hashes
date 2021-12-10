e8cace885d82cbba253b52cf169a87da43e4eb7c
Noah <34389972+nlanson@users.noreply.github.com>
2021-12-10 19:59:22 +1100

    write functions

--- src/core.rs
+++ src/core.rs
@@ -18,49 +18,34 @@ pub trait Sha2Engine {
 /// Functions module includes functions that are used by the Sha2 hashing family.
 pub mod functions {
     /// Choice
-    /// (X & Y ) ^
-    /// (!X & Z)
+    /// For each bit, if X is set, choose Y else choose Z.
     pub fn choice(x: u32, y: u32, z: u32) -> u32 {
-        todo!();
+        (x&y) ^ (!x&z)
     }
 
     /// Majority
-    /// (X & Y) ^
-    /// (X & Z) ^
-    /// (Y & Z)
+    /// Takes which ever bit is the majority of the three
     pub fn majority(x: u32, y: u32, z: u32) -> u32 {
-        todo!();
+        (x&y) ^ (x&z) ^ (y&z)
     }
 
     /// Uppercase Sigma 0 (Σ0)
-    /// RotR(X, 2)  ^
-    /// RotR(X, 13) ^
-    /// RotR(X, 22)
     pub fn usigma0(x: u32) -> u32 {
-        todo!();
+        x.rotate_right(2) ^ x.rotate_right(13) ^ x.rotate_right(22)
     }
 
     /// Uppercase Sigma 1 (Σ1)
-    /// RotR(X, 6)  ^
-    /// RotR(X, 11) ^
-    /// RotR(X, 25)
     pub fn usigma1(x: u32) -> u32 {
-        todo!();
+        x.rotate_right(6) ^ x.rotate_right(11) ^ x.rotate_right(25)
     }
 
     /// Lowercase Sigma 0 (σ0)
-    /// RotR(X, 7)  ^
-    /// RotR(X, 18) ^
-    /// ShR(X, 3)
     pub fn lsigma0(x: u32) -> u32 {
-        todo!();
+        x.rotate_right(6) ^ x.rotate_right(11) ^ (x>>3)
     }
 
     /// Lowercase Sigma 1 (σ1)
-    /// RotR(X, 17) ^
-    /// RotR(X, 19) ^
-    /// ShR(X, 10)
     pub fn lsigma1(x: u32) -> u32 {
-        todo!();
+        x.rotate_right(17) ^ x.rotate_right(19) ^ (x>>10)
     }
 }
