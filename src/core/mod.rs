bee1f78e2b64797653af8e8f073fa14895e199e3
Noah <34389972+nlanson@users.noreply.github.com>
2021-12-11 11:09:53 +1100

    Primitive trait

--- src/core/mod.rs
+++ src/core/mod.rs
@@ -3,10 +3,12 @@
 // Where all the core parts of the Sha2 hash function family reside.
 // This can be done because most parts are shared between the different
 // hash functions.
-//
-// Modules:
+
 pub mod message;
 pub mod functions;
+use std::ops::{
+    Add, Rem, BitXor, BitAnd, Not, Shr
+};
 
 
 
@@ -18,4 +20,35 @@ pub trait Sha2Engine {
 
     /// Complete the hash with the inputted data.
     fn hash<const N: usize>(self) -> [u8; N];
+}
+
+pub trait Primitive:
+    Into<u128> +
+    Add +
+    Rem +
+    BitAnd<Output = Self> +
+    BitXor<Output = Self> +
+    Not<Output = Self> +
+    Shr<usize, Output = Self> +
+    Copy
+{
+    fn rotate_right(&self, bits: usize) -> Self;
+
+    fn add_mod<T: Primitive + From<u128>>(a: T, b: T, mod_pow: u32) -> T {
+        let a: u128 = a.into();
+        let b: u128 = b.into();
+        T::from((a+b) % 2u128.pow(mod_pow))
+    }
+}
+
+impl Primitive for u32{ 
+    fn rotate_right(&self, bits: usize) -> Self {
+        self.rotate_right(bits)
+    }
+}
+
+impl Primitive for u64{
+    fn rotate_right(&self, bits: usize) -> Self {
+        self.rotate_right(bits)
+    }
 }
