4fdd4c68c8827ea54d12c818fd230f73e8bd99e5
Noah <34389972+nlanson@users.noreply.github.com>
2021-12-15 10:38:39 +1100

    move sha2 hash functions into single module

--- src/core/mod.rs
+++ src/core/mod.rs
@@ -19,7 +19,8 @@ use std::ops::{
 /// Generic paramters
 ///     T: u32 or u64 to indicate how many bits each word has
 ///     D: The size of the final hash in bytes
-///     S: The size of the message blocks in bytes
+///     S: The size of the message blocks in bytes,
+///     L: Bits reserved for length extention attacks when padding
 ///     W: The amount of words in each message schedule,
 pub trait HashEngine<T: Primitive, const D: usize, const S: usize, const L: usize, const W: usize>: Pad<S, L> {
     fn new() -> Self;
@@ -39,82 +40,7 @@ pub trait HashEngine<T: Primitive, const D: usize, const S: usize, const L: usiz
     fn hash(self) -> [u8; D];
 }
 
-/// Macro to implement the different hash functions with differnet parameters.
-/// 
-/// ### Params
-///  - Hash function name (Identifier)
-///  - Bits to operate on (Type u32 or u64)
-///  - Digest size in bytes (Number)
-///  - Pad size indicating the length of the length appended to the padding (Number)
-///  - Block size in bytes (Number)
-///  - Schedule length as count (Number)
-///  - Ignored state registers when concatenating the final state (Number)
-///  - Initial constants (Array of 8 integers of the same type specified in $bits)
-///  - Round constants (Array of $schedule_length of the same type specified in $bits)
-macro_rules! hash_function {
-    (
-        $name: ident,
-        $bits: ty,
-        $digest_size: expr,
-        $pad_size: expr,
-        $block_size: expr,
-        $schedule_length: expr,
-        $ignored_state: expr,
-        $initial_constants: expr,
-        $round_constants: expr
-    ) => {
-        pub struct $name {
-            input: Vec<u8>
-        }
 
-        impl HashEngine<$bits, $digest_size, $block_size, $pad_size, $schedule_length> for $name {
-            fn new() -> Self {
-                $name {
-                    input: vec![]
-                }
-            }
-
-            fn input<I>(&mut self, data: I)
-            where I: AsRef<[u8]>
-            {
-                self.input.extend_from_slice(data.as_ref());
-            }
-
-            fn read_input(self) -> Vec<u8> {
-                self.input
-            }
-
-            fn initial_constants() -> [$bits; 8] {
-                $initial_constants
-            }
-
-            fn round_constants() -> [$bits; $schedule_length] {
-                $round_constants
-            }
-
-            fn hash(self) -> [u8; $digest_size] {
-                let input = self.read_input();
-                let message: Message<$block_size> = Self::pad(input);
-                let blocks: Vec<MessageBlock<$block_size>> = MessageBlock::from_message(message);
-                let mut state: State<$bits> = State::new(Self::initial_constants());
-                for block in blocks {
-                    let schedule: MessageSchedule<$bits, $schedule_length> = MessageSchedule::from(block);
-                    state.compress(schedule, Self::round_constants());
-                }
-
-                let state = &state.read()[0..state.read().len()-$ignored_state];
-                let mut digest = vec![];
-                for i in 0..state.len() {
-                    digest.extend(state[i].to_be_bytes());
-                }
-                digest.try_into().expect("Bad digest")
-            }
-        }
-
-        impl Pad<$block_size, $pad_size> for $name { }
-    };
-}
-pub(crate) use hash_function;
 
 pub trait Primitive:
     Into<u128> + 
---
+++ src/sha2.rs
@@ -0,0 +1,191 @@
+// The SHA224 module is where the SHA224 hash function is implemented.
+//
+use crate::{
+    core::{
+        HashEngine,
+        message::{
+            Pad,
+            Message,
+            MessageBlock,
+            MessageSchedule
+        },
+        state::{
+            State,
+            Compression
+        }
+    },
+    constants::{
+        SHA224_INITIAL_CONSTANTS,
+        SHA256_INITIAL_CONSTANTS,
+        SHA384_INITIAL_CONSTANTS,
+        SHA512_INITIAL_CONSTANTS,
+        SHA256_ROUND_CONSTANTS,
+        SHA512_ROUND_CONSTANTS
+    }
+};
+use std::convert::TryInto;
+
+/// Macro to implement the different SHA2 hash functions. Macro params are the parameters.
+/// 
+/// ### Params
+///  - Hash function name (Identifier)
+///  - Bits to operate on (Type u32 or u64)
+///  - Digest size in bytes (Number)
+///  - Pad size indicating how many bytes should be reserved for the length extention attack preventer (Number)
+///  - Block size in bytes (Number)
+///  - Schedule length as count (Number)
+///  - Ignored state registers when concatenating the final state (Number)
+///  - Initial constants (Array of 8 integers of the same type specified in $bits)
+///  - Round constants (Array of $schedule_length of the same type specified in $bits)
+macro_rules! hash_function {
+    (
+        $name: ident,
+        $bits: ty,
+        $digest_size: expr,
+        $pad_size: expr,
+        $block_size: expr,
+        $schedule_length: expr,
+        $ignored_state: expr,
+        $initial_constants: expr,
+        $round_constants: expr
+    ) => {
+        pub struct $name {
+            input: Vec<u8>
+        }
+
+        impl HashEngine<$bits, $digest_size, $block_size, $pad_size, $schedule_length> for $name {
+            fn new() -> Self {
+                $name {
+                    input: vec![]
+                }
+            }
+
+            fn input<I>(&mut self, data: I)
+            where I: AsRef<[u8]>
+            {
+                self.input.extend_from_slice(data.as_ref());
+            }
+
+            fn read_input(self) -> Vec<u8> {
+                self.input
+            }
+
+            fn initial_constants() -> [$bits; 8] {
+                $initial_constants
+            }
+
+            fn round_constants() -> [$bits; $schedule_length] {
+                $round_constants
+            }
+
+            fn hash(self) -> [u8; $digest_size] {
+                let input = self.read_input();
+                let message: Message<$block_size> = Self::pad(input);
+                let blocks: Vec<MessageBlock<$block_size>> = MessageBlock::from_message(message);
+                let mut state: State<$bits> = State::new(Self::initial_constants());
+                for block in blocks {
+                    let schedule: MessageSchedule<$bits, $schedule_length> = MessageSchedule::from(block);
+                    state.compress(schedule, Self::round_constants());
+                }
+
+                let state = &state.read()[0..state.read().len()-$ignored_state];
+                let mut digest = vec![];
+                for i in 0..state.len() {
+                    digest.extend(state[i].to_be_bytes());
+                }
+                digest.try_into().expect("Bad digest")
+            }
+        }
+
+        impl Pad<$block_size, $pad_size> for $name { }
+    };
+}
+
+
+
+hash_function!(Sha224, u32, 28, 8, 64, 64, 1, SHA224_INITIAL_CONSTANTS, SHA256_ROUND_CONSTANTS);
+hash_function!(Sha256, u32, 32, 8, 64, 64, 0, SHA256_INITIAL_CONSTANTS, SHA256_ROUND_CONSTANTS);
+hash_function!(Sha384, u64, 48, 16, 128, 80, 2, SHA384_INITIAL_CONSTANTS, SHA512_ROUND_CONSTANTS);
+hash_function!(Sha512, u64, 64, 16, 128, 80, 0, SHA512_INITIAL_CONSTANTS, SHA512_ROUND_CONSTANTS);
+
+#[cfg(test)]
+mod tests {
+    use super::{HashEngine, Sha224, Sha256, Sha384, Sha512};
+
+    #[test]
+    fn sha224() {
+        let cases: Vec<(Vec<u8>, &str)> = vec![
+            (vec![0x61, 0x62, 0x63], "23097d223405d8228642a477bda255b32aadbce4bda0b3f7e36c9da7"),
+            (vec![], "d14a028c2a3a2bc9476102bb288234c415a2b01f828ea62ac5b3e42f"),
+            (b"abcdbcdecdefdefgefghfghighijhijkijkljklmklmnlmnomnopnopq".to_vec(), "75388b16512776cc5dba5da1fd890150b0c6455cb4f58b1952522525"),
+            (b"abcdefghbcdefghicdefghijdefghijkefghijklfghijklmghijklmnhijklmnoijklmnopjklmnopqklmnopqrlmnopqrsmnopqrstnopqrstu".to_vec(), "c97ca9a559850ce97a04a96def6d99a9e0e0e2ab14e6b8df265fc0b3")
+        ];
+        
+        
+        for case in cases {
+            let mut hasher = Sha224::new();
+            hasher.input(&case.0);
+            let digest = hasher.hash().iter().map(|x| format!("{:02x}", x)).collect::<String>();
+            assert_eq!(digest, case.1);
+        }
+    }
+
+    #[test]
+    fn sha256() {
+        let cases: Vec<(Vec<u8>, &str)> = vec![
+            (vec![0x61, 0x62, 0x63], "ba7816bf8f01cfea414140de5dae2223b00361a396177a9cb410ff61f20015ad"),
+            (vec![
+                0x61 ,0x62, 0x63, 0x64, 0x62, 0x63, 0x64, 0x65, 0x63, 0x64, 0x65, 0x66, 0x64, 0x65, 0x66, 0x67, 0x65, 0x66, 0x67, 0x68, 0x66, 0x67, 0x68, 0x69, 0x67,
+                0x68, 0x69, 0x6a, 0x68, 0x69, 0x6a, 0x6b, 0x69, 0x6a, 0x6b, 0x6c, 0x6a, 0x6b, 0x6c, 0x6d, 0x6b, 0x6c, 0x6d, 0x6e, 0x6c, 0x6d, 0x6e, 0x6f, 0x6d, 0x6e,
+                0x6f, 0x70, 0x6e, 0x6f, 0x70, 0x71],
+             "248d6a61d20638b8e5c026930c3e6039a33ce45964ff2167f6ecedd419db06c1"),
+            (vec![], "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855"),
+            (b"abcdbcdecdefdefgefghfghighijhijkijkljklmklmnlmnomnopnopq".to_vec(), "248d6a61d20638b8e5c026930c3e6039a33ce45964ff2167f6ecedd419db06c1"),
+            (b"abcdefghbcdefghicdefghijdefghijkefghijklfghijklmghijklmnhijklmnoijklmnopjklmnopqklmnopqrlmnopqrsmnopqrstnopqrstu".to_vec(), "cf5b16a778af8380036ce59e7b0492370b249b11e8f07a51afac45037afee9d1")
+        ];
+        
+        
+        for case in cases {
+            let mut hasher = Sha256::new();
+            hasher.input(&case.0);
+            let digest = hasher.hash().iter().map(|x| format!("{:02x}", x)).collect::<String>();
+            assert_eq!(digest, case.1);
+        }
+    }
+
+    #[test]
+    fn sha384() {
+        let cases: Vec<(Vec<u8>, &str)> = vec![
+            (vec![0x61, 0x62, 0x63], "cb00753f45a35e8bb5a03d699ac65007272c32ab0eded1631a8b605a43ff5bed8086072ba1e7cc2358baeca134c825a7"),
+            (vec![], "38b060a751ac96384cd9327eb1b1e36a21fdb71114be07434c0cc7bf63f6e1da274edebfe76f65fbd51ad2f14898b95b"),
+            (b"abcdbcdecdefdefgefghfghighijhijkijkljklmklmnlmnomnopnopq".to_vec(), "3391fdddfc8dc7393707a65b1b4709397cf8b1d162af05abfe8f450de5f36bc6b0455a8520bc4e6f5fe95b1fe3c8452b"),
+            (b"abcdefghbcdefghicdefghijdefghijkefghijklfghijklmghijklmnhijklmnoijklmnopjklmnopqklmnopqrlmnopqrsmnopqrstnopqrstu".to_vec(), "09330c33f71147e83d192fc782cd1b4753111b173b3b05d22fa08086e3b0f712fcc7c71a557e2db966c3e9fa91746039")
+        ];
+        
+        
+        for case in cases {
+            let mut hasher = Sha384::new();
+            hasher.input(&case.0);
+            let digest = hasher.hash().iter().map(|x| format!("{:02x}", x)).collect::<String>();
+            assert_eq!(digest, case.1);
+        }
+    }
+
+    #[test]
+    fn sha512() {
+        let cases: Vec<(Vec<u8>, &str)> = vec![
+            (vec![0x61, 0x62, 0x63], "ddaf35a193617abacc417349ae20413112e6fa4e89a97ea20a9eeee64b55d39a2192992a274fc1a836ba3c23a3feebbd454d4423643ce80e2a9ac94fa54ca49f"),
+            (vec![], "cf83e1357eefb8bdf1542850d66d8007d620e4050b5715dc83f4a921d36ce9ce47d0d13c5d85f2b0ff8318d2877eec2f63b931bd47417a81a538327af927da3e"),
+            (b"abcdbcdecdefdefgefghfghighijhijkijkljklmklmnlmnomnopnopq".to_vec(), "204a8fc6dda82f0a0ced7beb8e08a41657c16ef468b228a8279be331a703c33596fd15c13b1b07f9aa1d3bea57789ca031ad85c7a71dd70354ec631238ca3445"),
+            (b"abcdefghbcdefghicdefghijdefghijkefghijklfghijklmghijklmnhijklmnoijklmnopjklmnopqklmnopqrlmnopqrsmnopqrstnopqrstu".to_vec(), "8e959b75dae313da8cf4f72814fc143f8f7779c6eb9f7fa17299aeadb6889018501d289e4900f7e4331b99dec4b5433ac7d329eeb6dd26545e96e55b874be909")
+        ];
+        
+        
+        for case in cases {
+            let mut hasher = Sha512::new();
+            hasher.input(&case.0);
+            let digest = hasher.hash().iter().map(|x| format!("{:02x}", x)).collect::<String>();
+            assert_eq!(digest, case.1);
+        }
+    }
+}
