ece74a4a19df37b11f94e853f44803a251d414d5
Noah <34389972+nlanson@users.noreply.github.com>
2021-12-23 11:55:03 +1100

    ripemd160 working

--- src/ripemd.rs
+++ src/ripemd.rs
@@ -1,3 +1,5 @@
+use std::convert::TryInto;
+
 use crate::{
     core::{
         message::{
@@ -34,18 +36,13 @@ impl Ripemd160 {
 }
 
 macro_rules! round {
-    ($func: ident, $a: expr, $b: expr, $c: expr, $d: expr, $e: expr, $word: expr, $const: expr, $rol: expr) => {        
-        $a = $a.wrapping_add($func($b, $c, $d).wrapping_add($word).wrapping_add($const));
-        $a = $a.rotate_left($rol).wrapping_add($e);
-        $c = $c.rotate_left(10);
-    };
-}
-
-macro_rules! round_block {
-    ($words: expr, $func: ident, $buf: expr, $word_ord: expr, $rol_vals: expr, $const: expr) => {
-        for x in 0..15 {
-            round!($func, *$buf[0], *$buf[1], *$buf[2], *$buf[3], *$buf[4], $words[$word_ord[x]].value, $const, $rol_vals[x]);
-            $buf.rotate_right(1);
+    ($words: expr, $func: ident, $buf: expr, $word_ord: expr, $rol_ord: expr, $const: expr) => {
+        let mut buf: [&mut u32; 5] = $buf;
+        for x in 0..16 {
+            *buf[0] = buf[0].wrapping_add( $func(*buf[1], *buf[2], *buf[3]) ).wrapping_add($words[$word_ord[x]].value).wrapping_add($const);
+            *buf[0] = buf[0].rotate_left($rol_ord[x]).wrapping_add(*buf[4]);
+            *buf[2] = buf[2].rotate_left(10);
+            buf.rotate_right(1);
         }
     };
 }
@@ -62,29 +59,9 @@ impl HashEngine for Ripemd160 {
     fn hash(&self) -> Self::Digest {
         // The RIPEMD160 page:
         // https://homes.esat.kuleuven.be/~bosselae/ripemd160.html#:~:text=RIPEMD%2D160%20is%20a%20160,Antoon%20Bosselaers%2C%20and%20Bart%20Preneel.&text=A%20128%2Dbit%20hash%20result,19%20evaluations%20of%20the%20function.
-        //
-        // steps:
-        // 1. Pad into t 16 word blocks where each word is 32 bits
-        // 2. Do the compression which consists of:
-        //      Initializing a buffer with 5 constants.
-        //      Then for each block, copy the buffer values into variables (A,B,C,D,E) and (A',B',C',D',E')
-        //      and run 80 rounds of compression on each set of variables. The round details can be found in
-        //      the above link.
-        //      The primed and non-primed set of variables run through different compression in parallel.
-        //
-        //      The buffer is then set to the following
-        //          D' += C + MDbuf[1];
-        //          MDbuf[1] = MDbuf[2] + D + E';
-        //          MDbuf[2] = MDbuf[3] + E + A';
-        //          MDbuf[3] = MDbuf[4] + A + B';
-        //          MDbuf[4] = MDbuf[0] + B + C';
-        //          MDbuf[0] = D';
-        //      where '+' denotes integer addition modulo 32.
-        //
-        //      The next block follows...
 
         let message: Message<64> = Message::new(self.pad_input());
-        let blocks: Vec<MessageBlock<64>> = MessageBlock::from_message(message);
+        let blocks: Vec<MessageBlock<{Self::BLOCKSIZE}>> = MessageBlock::from_message(message);
         let mut mdbuf: State<u32, 5> = State::init(RIPEMD160_INITIAL_CONSTANTS);
         for block in blocks {
             let words: [Word<u32>; 16] = MessageSchedule::from(block).0;
@@ -93,7 +70,7 @@ impl HashEngine for Ripemd160 {
             let (mut aaa, mut bbb, mut ccc, mut ddd, mut eee) = (buffer[0], buffer[1], buffer[2], buffer[3], buffer[4]);
 
             /* round 1 */
-            round_block!(
+            round!(
                 words, f, [&mut aa, &mut bb, &mut cc, &mut dd, &mut ee],
                 [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15],
                 [11, 14, 15, 12, 5, 8, 7, 9, 11, 13, 14, 15, 6, 7, 9, 8],
@@ -101,7 +78,7 @@ impl HashEngine for Ripemd160 {
             );
 
             /* round 2 */
-            round_block!(
+            round!(
                 words, g, [&mut ee, &mut aa, &mut bb, &mut cc, &mut dd],
                 [7, 4, 13, 1, 10, 6, 15, 3, 12, 0, 9, 5, 2, 14, 11, 8],
                 [7, 6, 8, 13, 11, 9, 7, 15, 7, 12, 15, 9, 11, 7, 13, 12],
@@ -109,7 +86,7 @@ impl HashEngine for Ripemd160 {
             );
 
             /* round 3 */
-            round_block!(
+            round!(
                 words, h, [&mut dd, &mut ee, &mut aa, &mut bb, &mut cc],
                 [3, 10, 14, 4, 9, 15, 8, 1, 2, 7, 0, 6, 13, 11, 5, 12],
                 [11, 13, 6, 7, 14, 9, 13, 15, 14, 8, 13, 6, 5, 12, 7, 5],
@@ -117,7 +94,7 @@ impl HashEngine for Ripemd160 {
             );
 
             /* round 4 */
-            round_block!(
+            round!(
                 words, i, [&mut cc, &mut dd, &mut ee, &mut aa, &mut bb],
                 [1, 9, 11, 10, 0, 8, 12, 4, 13, 3, 7, 15, 14, 5, 6, 2],
                 [11, 12, 14, 15, 14, 15, 9, 8, 9, 14, 5, 6, 8, 6, 5, 12],
@@ -125,27 +102,73 @@ impl HashEngine for Ripemd160 {
             );
 
             /* round 5 */
-            round_block!(
+            round!(
                 words, j, [&mut bb, &mut cc, &mut dd, &mut ee, &mut aa],
                 [4, 0, 5, 9, 7, 12, 2, 10, 14, 1, 3, 8, 11, 6, 15, 13],
                 [9, 15, 5, 11, 6, 8, 13, 12, 5, 12, 13, 14, 11, 8, 5, 6],
                 0xa953fd4e
             );
 
-            // run paralell rounds JJJ through to FFF...
+            /* parallel round 1 */
+            round!(
+                words, j, [&mut aaa, &mut bbb, &mut ccc, &mut ddd, &mut eee],
+                [5, 14, 7, 0, 9, 2, 11, 4, 13, 6, 15, 8, 1, 10, 3, 12],
+                [8, 9, 9, 11, 13, 15, 15, 5, 7, 7, 8, 11, 14, 14, 12, 6],
+                0x50a28be6
+            );
+
+            /* parallel round 2 */
+            round!(
+                words, i, [&mut eee, &mut aaa, &mut bbb, &mut ccc, &mut ddd],
+                [6, 11, 3, 7, 0, 13, 5, 10, 14, 15, 8, 12, 4, 9, 1, 2],
+                [9, 13, 15, 7, 12, 8, 9, 11, 7, 7, 12, 7, 6, 15, 13, 11],
+                0x5c4dd124
+            );
+
+            /* parallel round 3 */
+            round!(
+                words, h, [&mut ddd, &mut eee, &mut aaa, &mut bbb, &mut ccc],
+                [15, 5, 1, 3, 7, 14, 6, 9, 11, 8, 12, 2, 10, 0, 4, 13],
+                [9, 7, 15, 11, 8, 6, 6, 14, 12, 13, 5, 14, 13, 13, 7, 5],
+                0x6d703ef3
+            );
+
+            /* parallel round 4 */
+            round!(
+                words, g, [&mut ccc, &mut ddd, &mut eee, &mut aaa, &mut bbb],
+                [8, 6, 4, 1, 3, 11, 15, 0, 5, 12, 2, 13, 9, 7, 10, 14],
+                [15, 5, 8, 11, 14, 14, 6, 14, 6, 9, 12, 9, 12, 5, 15, 8],
+                0x7a6d76e9
+            );
+
+            /* parallel round 5 */
+            round!(
+                words, f, [&mut bbb, &mut ccc, &mut ddd, &mut eee, &mut aaa],
+                [12, 15, 10, 4, 1, 5, 8, 7, 6, 2, 13, 14, 0, 3, 9, 11],
+                [8, 5, 12, 9, 12, 5, 14, 6, 8, 13, 6, 5, 15, 13, 11, 11],
+                0x00000000
+            );
 
             
-            //update state...
-            // ddd += cc + MDbuf[1];               /* final result for MDbuf[0] */
-            // MDbuf[1] = MDbuf[2] + dd + eee;
-            // MDbuf[2] = MDbuf[3] + ee + aaa;
-            // MDbuf[3] = MDbuf[4] + aa + bbb;
-            // MDbuf[4] = MDbuf[0] + bb + ccc;
-            // MDbuf[0] = ddd;
-            
+            // update state...
+            let new_state: [u32; 5] = [
+                ddd.wrapping_add(cc).wrapping_add(buffer[1]),
+                buffer[2].wrapping_add(dd).wrapping_add(eee),
+                buffer[3].wrapping_add(ee).wrapping_add(aaa),
+                buffer[4].wrapping_add(aa).wrapping_add(bbb),
+                buffer[0].wrapping_add(bb).wrapping_add(ccc)
+            ];
+            mdbuf.update(new_state);
         }
 
-        todo!();
+        mdbuf.read()
+            .iter()
+            .flat_map(|buf|
+                buf.to_le_bytes()
+            )
+            .collect::<Vec<u8>>()
+            .try_into()
+            .expect("Bad digest")
     }
 }
 
@@ -156,7 +179,9 @@ mod tests {
     #[test]
     fn ripemd160() {
         let mut e = Ripemd160::new();
-        e.input(b"abc");
-        e.hash();
+        e.input(b"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789");
+        let digest = e.hash().iter().map(|x| format!("{:02x}", x)).collect::<String>();
+        println!("{}", digest);
+        assert_eq!(digest, "b0e20b6e3116640286ed3a87a5713079b21f5189");
     }
 }
